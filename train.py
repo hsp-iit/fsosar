55dc3cc368e7fa3b0f0b212a222c6468553f0d47
Stefano Berti <stefano.berti@iit.it>
2025-02-13 14:20:26 +0000

    Fixed SSv2 its for SAFSAR

--- train.py
+++ train.py
@@ -57,7 +57,8 @@ def main(rank, world_size, model_name, data_name, os_loss):
             lr = 0.0001
             eval_after_steps = 20000
     elif model_name == "SAFSAR":
-        eval_after_steps = 30000
+        if data_name == "SSv2":
+            eval_after_steps = 35000
     config["eval_after_steps"] = eval_after_steps
     config["lr"] = lr
 

51ebecfb8abfd6b441e0b1585c3a0acc0bc37011
Stefano Berti <stefano.berti@iit.it>
2025-02-17 15:31:11 +0000

    Batch of jobs

--- train_safsar.sh
+++ train_safsar.sh
@@ -4,9 +4,11 @@
 #PBS -N safsar
 #PBS -q gpu
 #PBS -j oe
+#PBS -J 0-4
 
+list=(softmax eos gc discriminator)
 /applications/sw/miniforge/condabin/conda init bash
 source /home/sberti/.bashrc
 conda activate fsosar
 cd /home/sberti/fsosar
-python train.py --model SAFSAR --data SSv2 --os_loss softmax
+python train.py --model SAFSAR --data SSv2 --os_loss ${list[$PBS_ARRAY_INDEX]}

7a8bc422ad54096376461c4a3f85f133e10b1293
Stefano.Berti@iit.it <sberti@fe01.priv.franklin.lan>
2025-02-20 00:42:15 +0100

    boh

--- train_safsar.sh
+++ train_safsar.sh
@@ -2,11 +2,11 @@
 #PBS -l select=1:ncpus=10:ngpus=4
 #PBS -l walltime=24:00:00
 #PBS -N safsar
-#PBS -q gpu
+#PBS -q R914337
 #PBS -j oe
 
 /applications/sw/miniforge/condabin/conda init bash
 source /home/sberti/.bashrc
 conda activate fsosar
 cd /home/sberti/fsosar
-python train.py --model SAFSAR --data HMDB51 --os_loss softmax
+python train.py --model SAFSAR --data NTURGBD120 --os_loss discriminator

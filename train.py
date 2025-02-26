Fixed softmax metrics

train.py
@@ -239,19 +239,22 @@ def main(rank, world_size, model_name, data_name, os_loss, port):
                     mss = torch.nn.functional.softmax(similarity_matrix, dim=-1).amax(dim=-1).detach().cpu().numpy()
                     mls = similarity_matrix.amax(dim=-1).detach().cpu().numpy()
                     os_sim = similarity_matrix.detach().cpu().numpy()
-                    metrics = {"fs_acc": compute_accuracy(similarity_matrix[known_indices], all_labels[known_indices]),
-                            "os_auroc_mss": roc_auc_score(os_target, mss),
-                            "os_auroc_mls": roc_auc_score(os_target, mls),
-                            "os_auroc_mls_scaled": roc_auc_score(os_target, mssm),
-                            "os_aupr_mss": compute_aupr(os_target, mss),
-                            "os_aupr_mls": compute_aupr(os_target, mls),
-                            "os_aupr_mls_scaled": compute_aupr(os_target, mssm),
-                            "os_oscr_mss": compute_oscr(all_labels.detach().cpu().numpy(), os_sim, mss),
-                            "os_oscr_mls": compute_oscr(all_labels.detach().cpu().numpy(), os_sim, mls),
-                            "os_oscr_mls_scaled": compute_oscr(all_labels.detach().cpu().numpy(), os_sim, mssm),
-                            "os_acc_mss": ((torch.tensor(mss).cuda()>0.5) == torch.tensor(os_target).cuda()).sum().item()/len(os_target),
-                            "os_acc_mls": ((torch.tensor(mls).cuda()>0.5) == torch.tensor(os_target).cuda()).sum().item()/len(os_target),
-                            "os_acc_mls_scaled": ((torch.tensor(mssm).cuda()>0.5) == torch.tensor(os_target).cuda()).sum().item()/len(os_target)}
+                    if len(np.unique(os_target)) == 2:  # we need 2 classes to define open-set metrics, softmax train doesn't have them
+                        metrics = {"fs_acc": compute_accuracy(similarity_matrix[known_indices], all_labels[known_indices]),
+                                "os_auroc_mss": roc_auc_score(os_target, mss),
+                                "os_auroc_mls": roc_auc_score(os_target, mls),
+                                "os_auroc_mls_scaled": roc_auc_score(os_target, mssm),
+                                "os_aupr_mss": compute_aupr(os_target, mss),
+                                "os_aupr_mls": compute_aupr(os_target, mls),
+                                "os_aupr_mls_scaled": compute_aupr(os_target, mssm),
+                                "os_oscr_mss": compute_oscr(all_labels.detach().cpu().numpy(), os_sim, mss),
+                                "os_oscr_mls": compute_oscr(all_labels.detach().cpu().numpy(), os_sim, mls),
+                                "os_oscr_mls_scaled": compute_oscr(all_labels.detach().cpu().numpy(), os_sim, mssm),
+                                "os_acc_mss": ((torch.tensor(mss).cuda()>0.5) == torch.tensor(os_target).cuda()).sum().item()/len(os_target),
+                                "os_acc_mls": ((torch.tensor(mls).cuda()>0.5) == torch.tensor(os_target).cuda()).sum().item()/len(os_target),
+                                "os_acc_mls_scaled": ((torch.tensor(mssm).cuda()>0.5) == torch.tensor(os_target).cuda()).sum().item()/len(os_target)}
+                    else:
+                        metrics = {"fs_acc": compute_accuracy(similarity_matrix[known_indices], all_labels[known_indices])}
                 else:  # explicit method
                     if os_loss == "discriminator":
                         os_score = logits["disc_prob"].squeeze(1)
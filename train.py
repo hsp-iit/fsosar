487d51b839dc70ca8b3e353aef1f7f98f29ff0e9
Stefano Berti <stefano.berti@iit.it>
2025-02-27 10:38:05 +0000

    lower lr for 5w1s

--- train.py
+++ train.py
@@ -63,7 +63,7 @@ def main(rank, world_size, model_name, data_name, os_loss, port):
     elif model_name == "SAFSAR":
         if config["shot"] == 1:
             if data_name in ["SSv2", "NTURGBD120", "Diving48"]:
-                lr = 4e-5  # SAFSAR 5w1s uses this
+                lr = 4e-7  # SAFSAR 5w1s uses this
             elif data_name in ["HMDB51", "UCF101"]:
                 lr = 1e-7
             disc_weight = 1

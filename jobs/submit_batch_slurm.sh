fd14e69d7f761aca6d5b194adbc380aaf92a7165
steb6 <sberti@DESKTOP-KTSL9SD.iit.local>
2025-09-16 17:07:59 +0200

    New env, new local config, submit jobs:

--- jobs/submit_batch_slurm.sh
+++ jobs/submit_batch_slurm.sh
@@ -27,13 +27,13 @@ jobs_with_reservation=16    # Number of jobs to submit with reservation
 jobs_without_reservation=6  # Number of jobs to submit without reservation
 
 # Define models to test (can be customized)
-models=("TAOSAR")
+models=("ActionCLIP")
 
 # Define datasets to test (can be customized)
-datasets=("SSv2" "HMDB51" "UCF101" "NTURGBD120" "Diving48")
+datasets=("SSv2" "NTURGBD120" "Diving48")
 
 # Define OS losses to test (can be customized)
-os_losses=("softmax" "eos" "discriminator" "gc")
+os_losses=("discriminator")
 
 # Base job script file name (SLURM version)
 job_script="train_batch_slurm.sh"
